5c2b1a8886089384ca96462688d1dfd83728e974
ayeshase <ayeshazayn313@gmail.com>
2023-07-09 20:14:32 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Alert from './components/Alert';
+
+
+function App() {
+const [Mode, SetMode] = useState('light');
+
+const [alert, setAlert] = useState(null);
+const showAlert = (message, type)=>{
+   setAlert({
+    msg: message,
+    type: type
+  
+   })
+}
+
+
+const toogleMode = ()=>{
+  if(Mode === 'light'){
+    SetMode('dark');
+document.body.style.backgroundColor = '#1e3953';
+showAlert("dark mode has been enabled", "success");
+  }
+  else{
+    SetMode('light');
+    document.body.style.backgroundColor = 'white';
+    showAlert("light mode has been enabled", "success");
+
+  }
+}
+
+  return (
+   <>
+   <Navbar title="Best Games Ever" mode={Mode} toogleMode={toogleMode}/>
+   <Alert alert={alert} />
+   <About heading="About Us"/>
+   </>
+  );
+}
+
+export default App;
