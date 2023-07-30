d5b3db8e62bede3b2135894b58295505073dd931
unknown <ayeshazayn313@gmail.com>
2023-07-30 16:29:29 +0500

    work on props

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import GameItems from './gameItems'
 export default class Games extends Component {
+ constructor(){
+  super();
+  console.log("hello hi");
+   this.setState ={
+    articles: this.articles
+}
+
+
+}
+
+
   render() {
     return (
       <div className='container'>
             <div className='row'>
             <div className='col-md-4'>
-    <GameItems title="Games" description="my descrpition" />
+    <GameItems title="Games" description="my descrpition" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/14CC5/production/_130298158_charisma.ai2.png "/>
       </div>
       </div>
       </div>
--- src/components/gameItems.js
+++ src/components/gameItems.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 
+
 export default class gameItems extends Component {
+
+
+
   render() {
-   let {title, description} = this.props
+   let {title, description, imageUrl} = this.props
 
 
     return (
-      <div><div className="card" style={{width: "18rem;"}}>
-      <img src="..." className="card-img-top" alt="..."/>
-      <div className="card-body">
+      <div><div className="card" style={{width: "18rem"}}>
+      <img src={imageUrl} className="card-img-top" alt="..."/>
+      <div className="card-body"> 
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
-        <a href="/" className="btn btn-primary">Read More</a>
+        <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
       </div>
     </div></div>
     )
