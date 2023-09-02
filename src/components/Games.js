639c1310617df79d25f5ce7e2d357869ebce8c59
unknown <ayeshazayn313@gmail.com>
2023-09-02 18:10:26 +0500

    added infinte scroll in game app

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import GameItems from './gameItems'
 import propTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 
 export default class Games extends Component {
 
   static defaultProps = {
-    pageSize: 10
+    pageSize: 12
   }
   static propTypes = {
     pageSize: propTypes.number
@@ -15,23 +16,23 @@ export default class Games extends Component {
 
   constructor() {
     super();
-    console.log("hello hi");
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     }
   }
   async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?q=games&apiKey=42377419c3fa48e9b28e861ee070db1c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+    const url = `https://newsapi.org/v2/everything?q=games&apiKey=42377419c3fa48e9b28e861ee070db1c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
     let parseData = await data.json();
-    console.log(parseData)
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
-      loading: false })
+      loading: false
+    })
 
   }
 
@@ -47,39 +48,58 @@ export default class Games extends Component {
 
   handleNextClick = async () => {
     this.setState({
-      page: this.state.page + 1 });
+      page: this.state.page + 1
+    });
     this.updateNews();
 
   }
 
-
+  fetchMoreData = async () => {
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?q=games&apiKey=42377419c3fa48e9b28e861ee070db1c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true })
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parseData.articles),
+      totalResults: parseData.totalResults,
+      loading: false
+    })
+  };
 
 
 
 
   render() {
     return (
-      <div className='container'>
-        <div className='row'>
-          {this.state.articles?.map((article) => {
+      <>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<h4>Loading...</h4>}
+        >
+        <div className='container'>
 
-            return <div className='col-md-4' key={article.url}>
+          <div className='row'>
 
-              <GameItems title={article.title ? article.title.slice(0, 35) : ""} description={article.description ? article.description.slice(0, 54) : ""}
-                imageUrl={article.urlToImage} newsUrl={article.url} author={article.author} date={article.publishedAt} source={article.source.name} />
-            </div>
+            {this.state.articles?.map((article) => {
+
+              return <div className='col-md-4' key={article.url}>
+
+                <GameItems title={article.title ? article.title.slice(0, 35) : ""} description={article.description ? article.description.slice(0, 54) : ""}
+                  imageUrl={article.urlToImage} newsUrl={article.url} author={article.author} date={article.publishedAt} source={article.source.name} />
+              </div>
 
 
-          })}
-        </div>
-        <div className='container d-flex justify-content-between'>
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previos</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
+            })}
+          </div>
+         </div>
+        </InfiniteScroll>
 
-        </div>
+    
 
-      </div>
-
+</>
     )
   }
 }
--- src/components/gameItems.js
+++ src/components/gameItems.js
@@ -14,7 +14,7 @@ export default class gameItems extends Component {
           <span className='position-absolute top-0 translate-middle badge rounded-pill bg-danger' style={{ left: '90%', zIndex: '1' }}>{source}
             <span className='visually-hidden'>Unread messages</span>
           </span>
-          <img src={!imageUrl ? "https://t3n.de/news/wp-content/uploads/2022/12/Immersion_Death_Trash.jpeg" : imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://cdn.mos.cms.futurecdn.net/YFXPFmdUmmozc4C4s8VzNe-1200-80.jpg" : imageUrl } className="card-img-top" alt="..." />
           <div className="card-body" >
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
